10f4600e55790d2e60aadc0cd67288e80d4adbe8
J-Eugenio <eugeniopb4@gmail.com>
2020-07-13 22:07:54 -0300

    ServeName-ChannelInfo

--- src/components/ServerName/styles.ts
+++ src/components/ServerName/styles.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
+import { ExpandMore } from 'styled-icons/material';
 
 export const Container = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-berween;
+  justify-content: space-between;
 
   padding: 0 11px 0 16px;
   background-color: var(--secondary);
@@ -12,11 +13,17 @@ export const Container = styled.div`
   z-index: 2;
 `;
 
-export const Title = styled.div`
+export const Title = styled.h1`
   font-size: 16px;
   font-weight: bold;
 
   color: var(--white);
 `;
 
-export const ExpandIcon = styled.div``;
+export const ExpandIcon = styled(ExpandMore)`
+  width:28px;
+  height: 28px;
+
+  color: var(--white);
+  cursor: pointer;
+`;
